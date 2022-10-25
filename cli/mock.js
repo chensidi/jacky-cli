81861dd37153c5ef71402dba0565d06bf69e0bde
JackyCheung <2541012495@qq.com>
2022-10-25 21:51:49 +0800

    feat: 完善mock功能，支持webacpk以及ts的mock文件类型

--- cli/mock.js
+++ cli/mock.js
@@ -5,40 +5,80 @@
 */
 
 const fs = require('fs')
+const { prompt } = require('inquirer')
+const ejs = require('ejs')
 const { resolve } = require('path')
 const { addDep } = require('./utils')
 
-module.exports = () => {
-  createMockDir()
-  makeIndex()
+module.exports = async () => {
+  const dirName = await createMockDir()
+  await makeIndex(dirName)
   addDep(['mockjs'], '-D')
 }
 
-function createMockDir() {
-  return fs.mkdirSync(
-    resolve(process.cwd(), 'mock')
-  )
+async function createMockDir() {
+  const { dirName } = await prompt([
+    {
+      type: 'input',
+      name: 'dirName',
+      message: '输入mock文件夹名称',
+      default: 'mock',
+    },
+  ])
+  fs.mkdirSync(resolve(process.cwd(), dirName))
+  return dirName
 }
 
-function makeIndex() {
-  const index = fs.readFileSync(
-    resolve(__dirname, './template/mock.ejs'),
+async function makeIndex(dirName) {
+  const { ext } = await prompt([
     {
-      encoding: 'utf-8'
-    }
-  )
-  fs.writeFileSync(
-    resolve(process.cwd(), './mock/index.js'),
-    index
-  )
-  const demo = fs.readFileSync(
-    resolve(__dirname, './template/mockDemo.ejs'),
+      type: 'list',
+      name: 'ext',
+      message: '选择存放的mock数据是使用那种语言',
+      default: 'js',
+      choices: [
+        {
+          name: 'JavaScript',
+          value: 'js',
+        },
+        {
+          name: 'TypeScript',
+          value: 'ts',
+        },
+      ],
+    },
+  ])
+  const { base } = await prompt([
     {
-      encoding: 'utf-8'
-    }
-  )
+      type: 'list',
+      name: 'base',
+      message: '选择基于哪种构建工具？',
+      default: 'vite',
+      choices: [
+        {
+          name: 'Vite',
+          value: 'vite',
+        },
+        {
+          name: 'Webpack',
+          value: 'webpack',
+        },
+      ],
+    },
+  ])
+  const index = fs.readFileSync(resolve(__dirname, './template/mock.ejs'), {
+    encoding: 'utf-8',
+  })
   fs.writeFileSync(
-    resolve(process.cwd(), './mock/demo.js'),
-    demo
+    resolve(process.cwd(), `./${dirName}/index.${ext}`),
+    ejs.render(index, {
+      ts: ext === 'ts',
+      vite: base === 'vite',
+      webpack: base === 'webpack',
+    })
   )
+  const demo = fs.readFileSync(resolve(__dirname, './template/mockDemo.ejs'), {
+    encoding: 'utf-8',
+  })
+  fs.writeFileSync(resolve(process.cwd(), `./${dirName}/demo.${ext}`), demo)
 }
